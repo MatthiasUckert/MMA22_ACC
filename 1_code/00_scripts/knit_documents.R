cd04768bd13416e48f6de9877608caf8b25c8726
MatthiasUckert <Matthias.Uckert@outlook.com>
2021-04-18 21:01:14 +0200

    large commit

--- 1_code/00_scripts/knit_documents.R
+++ 1_code/00_scripts/knit_documents.R
@@ -11,4 +11,17 @@ rmarkdown::render(
   
 )
 
-rmarkdown::render_site("docs/", )
+rmarkdown::render(
+  input = "1_code/02_get_arcom_data.Rmd",
+  output_format = rmarkdown::html_document(
+    toc = TRUE,
+    toc_depth = 2,
+    toc_float = TRUE,
+    keep_md = TRUE,
+    self_contained = TRUE
+  ),
+  output_dir = "docs/"
+  
+)
+
+rmarkdown::render_site("docs/")
